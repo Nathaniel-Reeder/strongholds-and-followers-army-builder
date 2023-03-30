b2b8d4abcf3c6db3498d92371e81f2d02a175550
Nathaniel Reeder <n.ash.reeder@gmail.com>
2023-03-30 12:37:24 -0600

    Updated controller.js to prevent SQL injection using replacement object in the options object of the sequelize.query. Army names can now include apostrophes.

--- server/controller.js
+++ server/controller.js
@@ -15,8 +15,16 @@ module.exports = {
         console.log(req.body)
         //destructure info from the req.body
         const {commander, armyName, ancestry, experience, equipment, unitType, size} = req.body
-
-        //Perform calculations to create the army 
+        let replacementObj = {
+            commander,
+            armyName,
+            ancestry,
+            experience,
+            equipment,
+            unitType,
+            size
+        }
+    //Perform calculations to create the army 
         let attack = 0
         let power = 0
         let defense = 0
@@ -30,86 +38,84 @@ module.exports = {
             SELECT traits_id
             FROM ancestry_traits 
             JOIN traits ON traits_id = traits.id
-            WHERE ancestry_id = ${ancestry}
-        `)
+            WHERE ancestry_id = :ancestry
+        `, {replacements: replacementObj})
         extractedTrait[0].forEach((obj) => {traits.push(obj.traits_id)})
     //Get trait costs out of the database
         let extractedTraitCosts = await sequelize.query(`
             SELECT cost
             FROM ancestry_traits 
             JOIN traits ON traits_id = traits.id
-            WHERE ancestry_id = ${ancestry}
-        `)
+            WHERE ancestry_id = :ancestry
+        `, {replacements: replacementObj})
         extractedTraitCosts[0].forEach((obj) => {traitCost.push(obj.cost)})
         console.log('Console Log is Here: ', traitCost)
 
     //adjust variables with modifiers collected from the database for ancestry
-        let ancAttackMod = await sequelize.query(`SELECT attack FROM ancestry WHERE id = ${ancestry};`)
+        let ancAttackMod = await sequelize.query(`SELECT attack FROM ancestry WHERE id = :ancestry;`, {replacements: replacementObj})
         attack += ancAttackMod[0][0].attack
         console.log('Ancestry attack: ', attack)
 
-        let ancPowerMod = await sequelize.query(`SELECT power FROM ancestry WHERE id = ${ancestry};`)
+        let ancPowerMod = await sequelize.query(`SELECT power FROM ancestry WHERE id = :ancestry;`, {replacements: replacementObj})
         power += ancPowerMod[0][0].power
 
-        let ancDefMod = await sequelize.query(`SELECT defense FROM ancestry WHERE id = ${ancestry};`)
+        let ancDefMod = await sequelize.query(`SELECT defense FROM ancestry WHERE id = :ancestry;`, {replacements: replacementObj})
         defense += ancDefMod[0][0].defense
 
-        let ancToughMod = await sequelize.query(`SELECT toughness FROM ancestry WHERE id = ${ancestry};`)
+        let ancToughMod = await sequelize.query(`SELECT toughness FROM ancestry WHERE id = :ancestry;`, {replacements: replacementObj})
         toughness += ancToughMod[0][0].toughness
 
-        let ancMoraleMod = await sequelize.query(`SELECT morale FROM ancestry WHERE id = ${ancestry};`)
+        let ancMoraleMod = await sequelize.query(`SELECT morale FROM ancestry WHERE id = :ancestry;`, {replacements: replacementObj})
         morale += ancMoraleMod[0][0].morale
 
     //adjust variables with info from the dtabase using the experience value (attack, toughness, morale)
 
-        let expAtkMod = await sequelize.query(`SELECT attack FROM experience WHERE id = ${experience};`)
+        let expAtkMod = await sequelize.query(`SELECT attack FROM experience WHERE id = :experience;`, {replacements: replacementObj})
         attack += expAtkMod[0][0].attack
         console.log('Experience attack: ', attack)
 
-        let expToughMod = await sequelize.query(`SELECT toughness FROM experience WHERE id = ${experience};`)
+        let expToughMod = await sequelize.query(`SELECT toughness FROM experience WHERE id = :experience;`, {replacements: replacementObj})
         toughness += expToughMod[0][0].toughness
 
-        let expMoraleMod = await sequelize.query(`SELECT morale FROM experience WHERE id = ${experience};`)
+        let expMoraleMod = await sequelize.query(`SELECT morale FROM experience WHERE id = :experience;`, {replacements: replacementObj})
         morale += expMoraleMod[0][0].morale
 
     //adjust variables with info from the database using the equipment value (power, defense)
 
-        let eqPwrMod = await sequelize.query(`SELECT power FROM equipment WHERE id = ${equipment};`)
+        let eqPwrMod = await sequelize.query(`SELECT power FROM equipment WHERE id = :equipment;`, {replacements: replacementObj})
         power += eqPwrMod[0][0].power
 
-        let eqDefMod = await sequelize.query(`SELECT defense FROM equipment WHERE id = ${equipment};`)
+        let eqDefMod = await sequelize.query(`SELECT defense FROM equipment WHERE id = :equipment;`, {replacements: replacementObj})
         defense += eqDefMod[0][0].defense
 
     //adjust variables with info from the databuse using the unitType value (attack, power, defense, toughness, morale, cost_mod)
 
-        let unitAtkMod = await sequelize.query(`SELECT attack FROM unit_type WHERE id = ${unitType};`)
+        let unitAtkMod = await sequelize.query(`SELECT attack FROM unit_type WHERE id = :unitType;`, {replacements: replacementObj})
         attack += unitAtkMod[0][0].attack
         console.log('Unit Type attack: ', attack)
 
-        let unitPwrMod = await sequelize.query(`SELECT power FROM unit_type WHERE id = ${unitType};`)
+        let unitPwrMod = await sequelize.query(`SELECT power FROM unit_type WHERE id = :unitType;`, {replacements: replacementObj})
         power += unitPwrMod[0][0].power
 
-        let unitDefMod = await sequelize.query(`SELECT defense FROM unit_type WHERE id = ${unitType};`)
+        let unitDefMod = await sequelize.query(`SELECT defense FROM unit_type WHERE id = :unitType;`, {replacements: replacementObj})
         defense += unitDefMod[0][0].defense
 
-        let unitToughMod = await sequelize.query(`SELECT toughness FROM unit_type WHERE id = ${unitType};`)
+        let unitToughMod = await sequelize.query(`SELECT toughness FROM unit_type WHERE id = :unitType;`, {replacements: replacementObj})
         toughness += unitToughMod[0][0].toughness
 
-        let unitMoraleMod = await sequelize.query(`SELECT morale FROM unit_type WHERE id = ${unitType};`)
+        let unitMoraleMod = await sequelize.query(`SELECT morale FROM unit_type WHERE id = :unitType;`, {replacements: replacementObj})
         morale += unitMoraleMod[0][0].morale
 
-        let unitCostMod = await sequelize.query(`SELECT cost_mod FROM unit_type WHERE id = ${unitType};`)
+        let unitCostMod = await sequelize.query(`SELECT cost_mod FROM unit_type WHERE id = :unitType;`, {replacements: replacementObj})
         unitCostMod = unitCostMod[0][0].cost_mod
-        console.log("UnitCostMod", unitCostMod)
 
     //adjust variables with info from the size value (size, cost_mod)
 
-        let sizeDice = await sequelize.query(`SELECT size FROM size WHERE id = ${size};`)
+        let sizeDice = await sequelize.query(`SELECT size FROM size WHERE id = :size;`, {replacements: replacementObj})
         sizeDice = sizeDice[0][0].size
 
-        let sizeCostMod = await sequelize.query(`SELECT cost_mod FROM size WHERE id = ${size};`)
+        let sizeCostMod = await sequelize.query(`SELECT cost_mod FROM size WHERE id = :size;`, {replacements: replacementObj})
         sizeCostMod = sizeCostMod[0][0].cost_mod
-        console.log('sizeCostMod', sizeCostMod)
 
     //calculate cost value per instructions in Strongholds and Followers book.
     //add up the bonuses to attack, power, defense, and toughness. Add morale bonus * 2.
@@ -129,22 +135,22 @@ module.exports = {
         console.log('COMMANDER CONSOLE LOG HERE: ', commander)
         let commandExistance = await sequelize.query(`
             SELECT EXISTS 
-            (SELECT name FROM commander WHERE name = '${commander}');
-        `)   
+            (SELECT name FROM commander WHERE name = :commander);
+        `, {replacements: replacementObj})   
 
         // If the commander does exist, get its ID, then insert data into the army table in the database
         if (commandExistance[0][0].exists) {
             //get commander ID
-            let extractId = await sequelize.query(`SELECT id FROM commander WHERE name = '${commander}'`)
+            let extractId = await sequelize.query(`SELECT id FROM commander WHERE name = :commander`, {replacements: replacementObj})
             let id = extractId[0][0].id
             //insert that id and other data already gathered into the army table
             await sequelize.query(`
                 INSERT INTO army (commander_id, name, cost, ancestry_id, experience_id, equipment_id, unit_type_id, attack, defense, power, toughness, morale, size_id)
                 VALUES
-                (${id} , '${armyName}', ${cost}, ${ancestry}, ${experience}, ${equipment}, ${unitType}, ${attack}, ${defense}, ${power}, ${toughness}, ${morale}, ${size});
-            `)
+                (${id} , :armyName, ${cost}, :ancestry, :experience, :equipment, :unitType, ${attack}, ${defense}, ${power}, ${toughness}, ${morale}, :size);
+            `, {replacements: replacementObj})
             // get the ID of the new army
-            let extractedArmyId = await sequelize.query(`SELECT id FROM army WHERE name = '${armyName}'`)
+            let extractedArmyId = await sequelize.query(`SELECT id FROM army WHERE name = :armyName`, {replacements: replacementObj})
                 extractedArmyId = extractedArmyId[0][0].id
             //Create an entry in army_traits for each trait the army has, linking the traits to the army.
             traits.forEach(async (traitId) => {
@@ -156,18 +162,18 @@ module.exports = {
         } else {
             //Create the commander in the database
             await sequelize.query(`
-                INSERT INTO commander (name) VALUES ('${commander}');
-            `)
+                INSERT INTO commander (name) VALUES (:commander);
+            `, {replacements: replacementObj})
             //Get the new commander's id
-            let extractedCmdId = await sequelize.query(`SELECT id FROM commander WHERE name = '${commander}'`)
+            let extractedCmdId = await sequelize.query(`SELECT id FROM commander WHERE name = :commander`, {replacements: replacementObj})
             extractedCmdId = extractedCmdId[0][0].id
             //insert the commander's id and other data into the army table
             await sequelize.query(`
                 INSERT INTO army (commander_id, name, cost, ancestry_id, experience_id, equipment_id, unit_type_id, attack, defense, power, toughness, morale, size_id)
-                VALUES (${extractedCmdId} , '${armyName}', ${cost}, ${ancestry}, ${experience}, ${equipment}, ${unitType}, ${attack}, ${defense}, ${power}, ${toughness}, ${morale}, ${size});
-            `)
+                VALUES (${extractedCmdId} , ':armyName, ${cost}, :ancestry, :experience, :equipment, :unitType, ${attack}, ${defense}, ${power}, ${toughness}, ${morale}, :size);
+            `, {replacements: replacementObj})
             // get the ID of the new army
-            let extractedArmyId = await sequelize.query(`SELECT id FROM army WHERE name = '${armyName}'`)
+            let extractedArmyId = await sequelize.query(`SELECT id FROM army WHERE name = :armyName`, {replacements: replacementObj})
                 extractedArmyId = extractedArmyId[0][0].id
             //Create an entry in army_traits for each trait the army has, linking the traits to the army.
             traits.forEach(async (traitId) => {
@@ -177,7 +183,7 @@ module.exports = {
                 `)
             })
         }
-        res.send('Here is your response!').status(200)
+        res.send().status(200)
     },
     viewAllArmies: (req, res) => {
 
@@ -192,3 +198,9 @@ module.exports = {
         
     }
 }
+
+// const getArmyObj = () => {
+//     let databaseResponse = sequelize.query(`
+//         SELECT commander, name, cost, 
+//     `)
+// }
